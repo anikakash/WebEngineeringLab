a4cb0c4b04394ab42b7f1a8c657bbd2f48a49461
anikakash <anik15-14210@diu.edu.bd>
2023-03-29 01:06:43 +0600

    Start PHP Coding

---
+++ Learn_PHP/01_output.php
@@ -0,0 +1,29 @@
+<?php
+// echo - output strings, nnumbers, html 
+// echo 123, 'Hello', 10.5; // this is code
+
+// print - works like echo, but cna only take in a single argument
+// print 123
+
+// print_r() - print single values and arrays 
+// print_r([1,2,3]);
+
+// var_dump() - Returns more info like data type and legth;
+// var_dump(true);
+
+// var_export() - Similar to var_dump(). Outputs a string representation of a variable .
+// var_export("Hello");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP 01</title>
+</head>
+<body>
+    <h1><?php echo "Learn About Printing Method"; ?></h1>
+</body>
+</html>
