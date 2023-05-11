1face3c625a7e3ddfd89417f6a15473c221d5cba
anikakash <anik15-14210@diu.edu.bd>
2023-05-11 09:26:22 +0600

    add crud app

---
+++ crud/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>CRUD Operations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+            <h1 id="main_title">CRUD Application IN PHP</h1>
+
+            <div class="container">
+                <h2>ALL STUDENTS</h2>
+            <table class="table table-hover table-bordered table-striped">
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                </tr>
+                <tr>
+                    <th>5</th>
+                    <th>Anik</th>
+                    <th>Akash</th>
+                    <th>23</th>
+                </tr>
+                <tr>
+                    <th>3</th>
+                    <th>Akash</th>
+                    <th>Dash</th>
+                    <th>23</th>
+                </tr>
+            </table>
+    </div>
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
